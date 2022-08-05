a68d2cdef4763f23e68c9f89c5295d040582d77a
Artem Nakhodkin <artemn@google.com>
2022-08-06 00:38:16 +0300

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+
+    private static final String GREETING_MESSAGE = "Hello world!";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(GREETING_MESSAGE);
     }
 }
