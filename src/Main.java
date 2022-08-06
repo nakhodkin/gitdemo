2262cc04d42208a5e62204ad6e2425cb39e37eeb
Artem Nakhodkin <artemn@google.com>
2022-08-06 22:08:54 +0300

    added .gitignore and ignore .class files yes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     private static final String GREETING_MESSAGE = "Hello world!";
 
     public static void main(String[] args) {
+        printGreeting();
+    }
+
+    private static void printGreeting() {
         System.out.println(GREETING_MESSAGE);
     }
 }
