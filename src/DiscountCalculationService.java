061611a6553d71b8a9ef2b77651e83e876203984
Artem Nakhodkin <nakhodkiin@gmail.com>
2022-08-07 20:52:51 +0300

    Added a method that calculates discount

--- src/DiscountCalculationService.java
+++ src/DiscountCalculationService.java
@@ -1,2 +1,7 @@
 public class DiscountCalculationService {
+
+    private static final double DEFAULT_DISCOUNT = 0.05;
+    public double calculateDiscount(double price) {
+        return price * DEFAULT_DISCOUNT;
+    }
 }
